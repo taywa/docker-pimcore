06cadb4382d48ef88df4a2f2a6c750bb730596c2
Yves Serrano <ys@taywa.ch>
2025-12-16 20:12:54 +0100

    splitting pimcore jobs

--- docker/pimcore/s6-overlay/services-setup.sh
+++ docker/pimcore/s6-overlay/services-setup.sh
@@ -12,7 +12,12 @@ fi
 
 START_PIMCORE_MESSENGER_CONSUME=${START_PIMCORE_MESSENGER_CONSUME:-NO}
 if [ "$START_PIMCORE_MESSENGER_CONSUME" = "YES" ]; then
-    touch /etc/s6-overlay/s6-rc.d/user/contents.d/pimcore-messenger-consume
+    touch /etc/s6-overlay/s6-rc.d/user/contents.d/pimcore-maintenance
+    touch /etc/s6-overlay/s6-rc.d/user/contents.d/pimcore-messenger-consume-core
+    touch /etc/s6-overlay/s6-rc.d/user/contents.d/pimcore-messenger-consume-image_optimize1
+    touch /etc/s6-overlay/s6-rc.d/user/contents.d/pimcore-messenger-consume-image_optimize2
+    touch /etc/s6-overlay/s6-rc.d/user/contents.d/pimcore-messenger-consume-maintenance
+    touch /etc/s6-overlay/s6-rc.d/user/contents.d/pimcore-messenger-consume-scheduled_tasks
 fi
 
 START_PIMCORE_CONSUME_ASSET_UPDATE=${START_PIMCORE_CONSUME_ASSET_UPDATE:-NO}
