5912ec123b5584fa25aa0fa6332f8065ced08327
Yves Serrano <ys@taywa.ch>
2025-12-18 22:32:04 +0100

    added extra consume task for autostart

--- docker/pimcore/s6-overlay/services-setup.sh
+++ docker/pimcore/s6-overlay/services-setup.sh
@@ -12,9 +12,12 @@ fi
 
 START_PIMCORE_MESSENGER_CONSUME=${START_PIMCORE_MESSENGER_CONSUME:-NO}
 if [ "$START_PIMCORE_MESSENGER_CONSUME" = "YES" ]; then
+    touch /etc/s6-overlay/s6-rc.d/user/contents.d/pimcore-messenger-consume-async
     touch /etc/s6-overlay/s6-rc.d/user/contents.d/pimcore-messenger-consume-core
     touch /etc/s6-overlay/s6-rc.d/user/contents.d/pimcore-messenger-consume-image_optimize1
     touch /etc/s6-overlay/s6-rc.d/user/contents.d/pimcore-messenger-consume-image_optimize2
+    touch /etc/s6-overlay/s6-rc.d/user/contents.d/pimcore-messenger-consume-image_optimize3
+    touch /etc/s6-overlay/s6-rc.d/user/contents.d/pimcore-messenger-consume-image_optimize4
     touch /etc/s6-overlay/s6-rc.d/user/contents.d/pimcore-messenger-consume-maintenance
     touch /etc/s6-overlay/s6-rc.d/user/contents.d/pimcore-messenger-consume-scheduled_tasks
 fi
